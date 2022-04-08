9204ebb3ea5b1c4bb4014d749625c8dde3128d6d
mesu-ai <mesu.s.ss@gmail.com>
2022-04-08 10:33:20 +0600

    initialize

---
+++ src/pages/homepage/Home.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Home = () => {
+    return (
+        <div>
+            <h2>Home Page</h2>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusantium, at laborum facere facilis voluptatem nulla architecto nesciunt. Quos molestias id non nam nulla explicabo inventore. Non accusamus sed atque voluptates vel exercitationem dolor magnam maiores amet voluptatem voluptate, labore sequi minus voluptatum porro nostrum reprehenderit? Deleniti harum facilis molestiae error.</p>
+            
+        </div>
+    );
+};
+
+export default Home;
---
+++ src/pages/notfind/NotFind.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const NotFind = () => {
+    return (
+        <div>
+            <h4>Page Not Found</h4>
+            
+        </div>
+    );
+};
+
+export default NotFind;
