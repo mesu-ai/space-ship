68583c277798d3b0d9c398d8bce53f80239459bd
mesu-ai <mesu.s.ss@gmail.com>
2022-04-09 11:25:16 +0600

    update banner

--- src/pages/homepage/Home.js
+++ src/pages/homepage/Home.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import Banner from './banner/Banner';
+import './Home.css';
 
 const Home = () => {
     return (
-        <div>
-            <Banner/>
-            
-        </div>
+        <>
+        <div className='bannerContainer'><Banner/></div>
+        </>
     );
 };
 
