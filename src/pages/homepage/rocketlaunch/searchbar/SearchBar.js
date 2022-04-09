31fdb9b70b27b411d0d38b6692e7771d5601eb30
mesu-ai <mesu.s.ss@gmail.com>
2022-04-09 17:02:08 +0600

    update rocketsearchbar

--- src/pages/homepage/rocketlaunch/searchbar/SearchBar.js
+++ src/pages/homepage/rocketlaunch/searchbar/SearchBar.js
@@ -1,6 +1,6 @@
-
 import React from 'react';
 import { Button, Container, Form, FormControl, Nav, Navbar, NavDropdown } from 'react-bootstrap';
+import './SearchBar.css';
 
 const SearchBar = () => {
     return (
@@ -8,30 +8,44 @@ const SearchBar = () => {
 
         <Navbar expand="lg" variant="dark" style={{backgroundColor:'#2D2853'}}>
         <Container>
-            <Navbar.Brand href="/">React-Bootstrap</Navbar.Brand>
+
             <Navbar.Toggle aria-controls="basic-navbar-nav" />
             <Navbar.Collapse id="basic-navbar-nav">
 
-            <Nav className="me-auto">
-                <Nav.Link >Home</Nav.Link>
-                <Nav.Link >Link</Nav.Link>
-                
-                <NavDropdown title="Dropdown" id="basic-nav-dropdown">
-                <NavDropdown.Item >Action</NavDropdown.Item>
-                <NavDropdown.Item >Another action</NavDropdown.Item>
-                <NavDropdown.Item >Something</NavDropdown.Item>
-                <NavDropdown.Divider />
-                <NavDropdown.Item >Separated link</NavDropdown.Item>
-                </NavDropdown>
+            <Nav className="me-auto ">
+
+
+            <div className='d-flex align-items-center'>
+                <label className='filterLabel'>Is upcoming?</label>
+                <li className='ms-4'>
+                <Form.Select aria-label="Is upcoming?" className='custom-select'>
+                    <option value="1">Yes</option>
+                    <option value="2">No</option>   
+                </Form.Select>
+                </li>
+            </div>
+
+            <span className='searchBarDivider mx-4 d-none d-md-block'></span>
+
+            <div className='d-flex align-items-center'>
+                <label className='filterLabel'>Launch year?</label>
+                <li className='ms-4'>
+                <Form.Select aria-label="Is upcoming?" className='custom-select'>
+                    <option value="1">Yes</option>
+                    <option value="2">No</option>   
+                </Form.Select>
+                </li>
+            </div>
+               
             </Nav>
             <Form className="d-flex">
                 <FormControl
                 type="search"
-                placeholder="Search"
-                className="me-2"
+                placeholder="Search for rocket"
+                className="searchField"
                 aria-label="Search"
                 />
-                <Button variant="outline-success">Search</Button>
+                <Button variant="dark" className='searchBtn px-4'>Search</Button>
             </Form>
 
             </Navbar.Collapse>
